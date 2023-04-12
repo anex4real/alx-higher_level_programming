9b52d1470b173216842eda7c333de1c6d7c199d2
Anu Atolagbe <anex4real@gmail.com>
2023-04-12 00:59:19 -0700

    all task done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
-'use strict';
-let nextMax = 0;
-let args = process.argv.slice(2);
-if (args.length > 1) {
-  args.sort();
-  nextMax = args[args.length - 2];
+
+const len = process.argv.length - 2;
+const array = process.argv.slice(2);
+if (len <= 1) {
+  console.log('0');
+} else {
+  array.sort((a, b) => {
+    return a - b;
+  });
+  console.log(array[len - 2]);
 }
-console.log(nextMax);
